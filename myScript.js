580451b2f5974877c4593ecfa6e1da781d26e508
DaxGute <36718376+DaxGute@users.noreply.github.com>
2021-01-14 20:16:00 -0800

    out it in google forms format and used parse and stuff

--- myScript.js
+++ myScript.js
@@ -1,10 +1,14 @@
 changedCred = document.getElementById("ChangeCred");
 enteredCred = document.getElementById("EnterCred");
 
-localStorage.setItem("email", "email");
-localStorage.setItem("password", "password");
+var pref = {
+  email: "none",
+  password: "none",
+  reaction: "none",
+  signUp: "no",
+}
 
-
+localStorage.setItem("preferences", JSON.stringify(pref));
 
 // Click event to attach to button
 function tryCred () {
@@ -14,7 +18,7 @@ function tryCred () {
   document.getElementById('changedEmail').value = "";
   document.getElementById('changedPassword').value = "";
 
-  if (inputEmail == localStorage.getItem("email") && inputPassword == localStorage.getItem("password")){
+  if (inputEmail == JSON.parse(localStorage.getItem("preferences")).email && inputPassword == JSON.parse(localStorage.getItem("preferences")).password){
     document.getElementById('wrong').style.display = "none";
     document.getElementById('correct').style.display = "block";
   }else{
@@ -24,33 +28,57 @@ function tryCred () {
 }
 
 function autofill(){
-  document.getElementById('changedEmail').value = localStorage.getItem("email");
-  document.getElementById('changedPassword').value = localStorage.getItem("password");
+  document.getElementById('changedEmail').value = JSON.parse(localStorage.getItem("preferences")).email;
+  document.getElementById('changedPassword').value = JSON.parse(localStorage.getItem("preferences")).password;
+  if (JSON.parse(localStorage.getItem("preferences")).signUp == "true"){
+    document.getElementById('emailSignup').checked = true;
+  }else{
+    document.getElementById('emailSignup').checked = false;
+  }
+  if (JSON.parse(localStorage.getItem("preferences")).reaction == "happy"){
+    document.getElementById('happy').checked = true;
+  }else if(JSON.parse(localStorage.getItem("preferences")).reaction == "meh"){
+    document.getElementById('meh').checked = true;
+  }else if(JSON.parse(localStorage.getItem("preferences")).reaction == "unhappy"){
+    document.getElementById('unhappy').checked = true;
+  }
 }
 
 function changeCred () {
-  localStorage.setItem("email", document.getElementById('newEmail').value);
-  localStorage.setItem("password", document.getElementById('newPassword').value);
+  pref.email = document.getElementById('newEmail').value;
+  pref.password = document.getElementById('newPassword').value
   document.getElementById('newEmail').value = "";
   document.getElementById('newPassword').value = "";
+  localStorage.setItem("preferences", JSON.stringify(pref));
 }
 
 function reactions () { //how to do radio buttons
   document.getElementById('myResponse').style.display = "block";
   if (document.getElementById('happy').checked){
     document.getElementById('myResponse').innerHTML = "YAY. AWESOME. ME TOO!";
+    pref.reaction = "happy";
   }
   else if (document.getElementById('meh').checked) {
     document.getElementById('myResponse').innerHTML = "I am sorry to hear that. If you are meh, I am too.";
+    pref.reaction = "meh";
   }
   else if (document.getElementById('unhappy').checked) {
     document.getElementById('myResponse').innerHTML = "Then leave...I never liked you anyways";
+    pref.reaction = "unhappy";
   }
+  localStorage.setItem("preferences", JSON.stringify(pref));
 }
 
 function checkEmail () {
-  document.getElementById('confirmMessage').style.display = "block";
-  window.setTimeout(checkEmailPtTwo, 8000);
+  if (document.getElementById('emailSignup').checked){
+    document.getElementById('confirmMessage').style.display = "block";
+    window.setTimeout(checkEmailPtTwo, 8000);
+    pref.signUp = "true";
+  }else{
+    pref.signUp = "false";
+  }
+
+  localStorage.setItem("preferences", JSON.stringify(pref));
 }
 
 function checkEmailPtTwo (){
