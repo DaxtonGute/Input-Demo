a22424a0f84058ffc9c335d1a972a055e87e99f4
DaxGute <36718376+DaxGute@users.noreply.github.com>
2021-01-25 16:47:30 -0800

    username and password added

--- formEditorScript.js
+++ formEditorScript.js
@@ -7,13 +7,46 @@ function newTextField() {
   var newLineOne = document.createElement("br");
   var input = document.createElement("input");
   input.type = "text";
-  input.placeholder = "don't type here";
+  input.placeholder = "This is a Text Field";
   input.style.width = "100%";
+  input.readOnly = true;
+  input.style.backgroundColor = "grey";
   var newLineTwo = document.createElement("p");
 
   insideThis.appendChild(label);
   insideThis.appendChild(newLineOne);
   insideThis.appendChild(input);
   insideThis.appendChild(newLineTwo);
+}
 
+function newUsernameField() { // lots of duplicate code
+  var label = document.createElement("p");
+  label.innerHTML = "Username:";
+  var input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "This is a Username Field";
+  input.style.width = "100%";
+  input.readOnly = true;
+  input.style.backgroundColor = "grey";
+  var newLine = document.createElement("p");
+
+  insideThis.appendChild(label);
+  insideThis.appendChild(input);
+  insideThis.appendChild(newLine);
+}
+
+function newPasswordField() { // lots of duplicate code
+  var label = document.createElement("p");
+  label.innerHTML = "Password:";
+  var input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "This is a Password Field";
+  input.style.width = "100%";
+  input.readOnly = true;
+  input.style.backgroundColor = "grey";
+  var newLine = document.createElement("p");
+
+  insideThis.appendChild(label);
+  insideThis.appendChild(input);
+  insideThis.appendChild(newLine);
 }
